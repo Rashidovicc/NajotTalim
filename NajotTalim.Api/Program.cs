870065eef219c09380954c17633ab75c85b0abb2
Akhmadjon <mrjohnaccaount@gmail.com>
2022-04-03 16:14:04 +0500

    removed unnecessary namespaces

--- NajotTalim.Api/Program.cs
+++ NajotTalim.Api/Program.cs
@@ -1,11 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace NajotTalim.Api
 {
@@ -22,5 +16,18 @@ namespace NajotTalim.Api
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        //public static IHostBuilder CreateHostBuilder(string[] args) =>
+        //   Host.CreateDefaultBuilder(args)
+        //       .ConfigureWebHostDefaults(webBuilder =>
+        //       {
+        //           webBuilder.UseStartup<Startup>();
+        //       }).
+        //       UseSerilog((hostingContext, loggingConfiguration) =>
+        //       {
+        //           loggingConfiguration.ReadFrom.Configuration(loggingConfiguration);
+        //       });
+
+
     }
 }
