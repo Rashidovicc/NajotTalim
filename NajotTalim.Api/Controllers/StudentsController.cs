15c909e60e3a32d20a3cb70d15cf75b1bc44c30a
Akhmadjon <mrjohnaccaount@gmail.com>
2022-04-04 06:05:31 +0500

    StudentController is ready

--- NajotTalim.Api/Controllers/StudentsController.cs
+++ NajotTalim.Api/Controllers/StudentsController.cs
@@ -1,4 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NajotTalim.Domain.Commons;
+using NajotTalim.Domain.Configurations;
+using NajotTalim.Domain.Entities.Students;
+using NajotTalim.Domain.Enums;
+using NajotTalim.Services.DTOs;
+using NajotTalim.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace NajotTalim.Api.Controllers
 {
@@ -6,6 +15,51 @@ namespace NajotTalim.Api.Controllers
     [Route("Api/[controller]")]
     public class StudentsController : ControllerBase
     {
-        
+        private readonly IStudentService studentService;
+        public StudentsController(IStudentService studentService)
+        {
+            this.studentService = studentService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<BaseResponse<Student>>> Create([FromForm] StudentForCreation studentDto)
+        {
+            var result = await studentService.CreateAsync(studentDto);
+
+            return StatusCode(result.Code ?? result.Error.Code.Value, result);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<BaseResponse<IEnumerable<Student>>>> GetAll([FromQuery] PaginationParams @params)
+        {
+            var result = await studentService.GetAllAsync(@params);
+
+            return StatusCode(result.Code ?? result.Error.Code.Value, result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BaseResponse<Student>>> Get([FromRoute] Guid id)
+        {
+            var result = await studentService.GetAsync(p => p.Id == id);
+
+            return StatusCode(result.Code ?? result.Error.Code.Value, result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<BaseResponse<Student>>> Update(Guid id, StudentForCreation studentDto)
+        {
+            var result = await studentService.UpdateAsync(id, studentDto);
+
+            return StatusCode(result.Code ?? result.Error.Code.Value, result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<BaseResponse<bool>>> Delete(Guid id)
+        {
+            var result = await studentService.DeleteAsync(p => p.Id == id && p.State != ItemState.Deleted);
+
+            return StatusCode(result.Code ?? result.Error.Code.Value, result);
+        }
+
     }
 }
--- NajotTalim.Api/Startup.cs
+++ NajotTalim.Api/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,6 +8,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using NajotTalim.Api.Extensions;
 using NajotTalim.Data.Contexts;
+using NajotTalim.Services.Helpers;
 using NajotTalim.Services.Mappers;
 
 namespace NajotTalim.Api
@@ -44,13 +46,17 @@ namespace NajotTalim.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "NajotTalim.Api v1"));
             }
 
+            if (app.ApplicationServices.GetService<IHttpContextAccessor>() != null)
+            {
+                HttpContextHelper.Accessor = app.ApplicationServices.GetRequiredService<IHttpContextAccessor>();
+            }
             app.UseHttpsRedirection();
 
             app.UseRouting();
